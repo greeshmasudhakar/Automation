da9357948f3c4d2692bbc74121985676c8025ea5
Greeshma Vakkalagadda <greeshmasudhakar11@gmail.com>
2024-09-08 10:57:30 +0530

    first commit

---
+++ src/test/java/httpmethods/GetMethod.java
@@ -0,0 +1,13 @@
+package httpmethods;
+
+import org.testng.annotations.Test;
+
+public class GetMethod {
+
+
+    @Test
+    public void testMethod(){
+        int a=1+2;
+        System.out.println("Testing jenkins"+a);
+    }
+}
