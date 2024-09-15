8d85a2aef65fa6bf123dd68990fcb1c8e54d8eb9
Greeshma Vakkalagadda <greeshmasudhakar11@gmail.com>
2024-09-15 15:42:50 +0530

    added testng.xml

--- src/test/java/httpmethods/GetMethod.java
+++ src/test/java/httpmethods/GetMethod.java
@@ -1,13 +1,19 @@
 package httpmethods;
 
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
 
 public class GetMethod {
 
 
     @Test
-    public void testMethod(){
-        int a=1+2;
-        System.out.println("Testing jenkins"+a);
+    public void getResponseBody(){
+        given().queryParam("CUSTOMER_ID",68195)
+                .queryParam("PASSWORD","1234!")
+                .queryParam("Account_No",1)
+                .when().get("http://demo.guru99.com/V4/sinkministatement.php").then().log().body();
     }
 }
