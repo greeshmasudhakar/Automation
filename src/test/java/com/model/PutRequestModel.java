21a363e8ee7319ff83762f97c2eb04b1296bda15
Greeshma Vakkalagadda <greeshmasudhakar11@gmail.com>
2025-01-04 14:39:43 +0530

    API Chaining

---
+++ src/test/java/com/model/PutRequestModel.java
@@ -0,0 +1,14 @@
+package com.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PutRequestModel {
+
+   private String place_id;
+    private String address;
+    private String key;
+
+}
--- src/test/java/com/utils/Utility.java
+++ src/test/java/com/utils/Utility.java
@@ -1,11 +1,12 @@
 package com.utils;
 
 import com.google.gson.Gson;
-import com.model.Location;
-import com.model.PostResponseModel;
-import com.model.Request;
+import com.model.*;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
@@ -33,6 +34,22 @@ public class Utility {
         Response response= given().log().all().baseUri("https://rahulshettyacademy.com").queryParam("key","qaclick123").header("content-type","application/json")
                 .body(requestBody).when().post(" /maps/api/place/add/json");
         System.out.println("response1:"+response.getBody().asString());
+        JSONObject jsonObject=new JSONObject(response.getBody().asString());
+        String place_id= jsonObject.getString("place_id");
+        Response response2= given().baseUri("https://rahulshettyacademy.com").queryParam("key","qaclick123").
+                queryParam("place_id",place_id).when().get(" /maps/api/place/get/json");
+        System.out.println("get response "+response2.getBody().asString());
+        PutRequestModel putPayload=new PutRequestModel();
+        putPayload.setPlace_id(place_id);
+        putPayload.setAddress("North America");
+        putPayload.setKey("qaclick123");
+        Response putResponse= given().log().all().baseUri("https://rahulshettyacademy.com").body(putPayload)
+                .when().put("/maps/api/place/update/json");
+        System.out.println("put response "+putResponse.getBody().asString());
+        DeleteRequest deletepayload=new DeleteRequest();
+        deletepayload.setPlace_id(place_id);
+        Response delete=given().baseUri("https://rahulshettyacademy.com").body(deletepayload).queryParam("key","qaclick123").when().delete(" /maps/api/place/delete/json");
+        System.out.println("delete response "+delete.getBody().asString());
         Gson gson=new Gson();
         return gson.fromJson(response.getBody().asString(),PostResponseModel.class);
     }
